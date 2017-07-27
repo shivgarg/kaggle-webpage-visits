0082d352721b8ec13ae67cd761f98c5fac64d66b
Shivam Garg <shivgarg@live.com>
2017-07-28 07:51:56 +0900

    added some more csv files and removed some bugs from scrapper

--- code/scrapper.py
+++ code/scrapper.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import webbrowser
 import time
 import os
@@ -13,13 +16,13 @@ import json
 import urllib
 from datetime import datetime, timedelta
 import requests
-
+from functools import reduce
 
 def get_buckets(start_date, end_date):
     start_date_dt = datetime.strptime(start_date, '%Y-%m-%d')
     end_date_dt = datetime.strptime(end_date, '%Y-%m-%d')
 
-    bucket_limits = [start_date_dt]
+    bucket_limits = [start_date_dt-timedelta(1)]
     left_limit = start_date_dt
     while left_limit <= end_date_dt:
         new_limit = left_limit + timedelta(days=181)
@@ -30,10 +33,9 @@ def get_buckets(start_date, end_date):
     return bucket_limits
 
 def get_data(bucket_start_date,bucket_end_date, keyword):
-    bucket_start_date_printed = datetime.strftime(bucket_start_date, '%Y-%m-%d')
+    bucket_start_date_printed = datetime.strftime(bucket_start_date+timedelta(1), '%Y-%m-%d')
     bucket_end_date_printed = datetime.strftime(bucket_end_date, '%Y-%m-%d')
     time_formatted = bucket_start_date_printed + '+' + bucket_end_date_printed
-#    print time_formatted
     req = {"comparisonItem":[{"keyword":keyword, "geo":geo, "time": time_formatted}], "category":category,"property":""}
     hl = ""
     tz = "-120"
@@ -92,15 +94,14 @@ def get_weekly_frame(start_date, end_date, keyword):
         print 'No need to stitch; your time interval is short enough. '
         return None
     else:
-        resp_text = get_data(datetime.strptime(start_date, '%Y-%m-%d'), datetime.strptime(end_date, '%Y-%m-%d'), keyword)
+        resp_text = get_data(datetime.strptime(start_date, '%Y-%m-%d')-timedelta(1), datetime.strptime(end_date, '%Y-%m-%d'), keyword)
         return parse_csv(get_csv(resp_text))
 
-def stitch_frames(daily_frames, weekly_frame):
+def stitch_frames(daily_frames, weekly_frame,start_date,end_date):
 
     daily_frame = pd.concat(daily_frames, ignore_index = True)
     daily_frame.columns = ['Date', 'Daily_Volume']
     pd.to_datetime(daily_frame['Date'])
-
     weekly_frame.columns = ['Week_Start_Date', 'Weekly_Volume']
     daily_frame.index = daily_frame['Date']
     weekly_frame.index = weekly_frame['Week_Start_Date']
@@ -114,13 +115,17 @@ def stitch_frames(daily_frames, weekly_frame):
 
     for i in range(0,len(bins)):
         week_start_date = datetime.strftime(bins[i][0],'%Y-%m-%d')
+	sum_interest = reduce((lambda x, y: float(x) + float(y)), daily_frame['Daily_Volume'].loc[week_start_date:datetime.strftime(bins[i][len(bins[i])-1],'%Y-%m-%d')],0)
         for j in range(0,len(bins[i])):
+	    if bins[i][j] > datetime.strptime(end_date,'%Y-%m-%d'):
+		break
             this_date = datetime.strftime(bins[i][j],'%Y-%m-%d')
-            try:
-                this_val = int(float(weekly_frame['Weekly_Volume'][week_start_date])*float(daily_frame['Daily_Volume'][this_date])/float(daily_frame['Daily_Volume'][week_start_date]))
+            if sum_interest !=0:
+                this_val = float(weekly_frame['Weekly_Volume'][week_start_date])*float(daily_frame['Daily_Volume'][this_date])/float(sum_interest)
                 final_data[this_date] = this_val
-            except:
-                pass
+            else:
+		final_data[this_date] = 0.0
+    
     final_data_frame = DataFrame.from_dict(final_data,orient='index').sort_index()
 #   final_data_frame[0] = np.round(final_data_frame[0]/final_data_frame[0].max()*100,2)
 
@@ -130,18 +135,17 @@ def stitch_frames(daily_frames, weekly_frame):
     final_data_frame.to_csv('csv/{0}.csv'.format(keywords.replace('+','').replace('/','')), sep=',')
 
 
-
 f=open(sys.argv[1])
 f=csv.reader(f)
 next(f)
 for row in f:
     keywords=row[0].split('_')
-    filename='csv/'+''.join(keywords[0:len(keywords)-3]).replace('/','').replace('File:','')+'.csv'
+    filename='csv/'+''.join(keywords[0:len(keywords)-3]).replace('/','').replace('File:','').replace('.jpg','').replace('User:','')+'.csv'
     if os.path.isfile(filename):
 	continue
-    keywords='+'.join(keywords[0:len(keywords)-3]).replace('File:','')
+    keywords='+'.join(keywords[0:len(keywords)-3]).replace('File:','').replace('.jpg','').replace('User:','')
     start_date = '2015-03-01'
-    end_date = '2017-03-31'
+    end_date = '2017-07-20'
     geo = ''
     category = 0
     print keywords
@@ -155,5 +159,6 @@ for row in f:
 		if not frame.empty:
 			empty=False
     if not empty:
-    	stitch_frames(daily_frames, weekly_frame)
+	print "stitching"
+    	stitch_frames(daily_frames, weekly_frame,start_date,end_date)
 
